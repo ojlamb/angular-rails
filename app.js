81929b0a5733c4a6745d5a47cb8fa5c85a0c45e2
Owen Lamb <owen.lamb@gmail.com>
2015-11-10 15:42:52 -0800

    added ui-router and moved html into template

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}])
 
 .factory('posts', [function(){
   var o = {
@@ -6,19 +26,32 @@ angular.module('flapperNews', [])
   };
   return o;
 }])
-.controller('MainCtrl', ['$scope','posts',function($scope, posts){
-    $scope.posts = posts.posts;
-    $scope.addPost = function(){
-      if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({
-        title: $scope.title,
-        link: $scope.link,
-        upvotes: 0});
-      $scope.title = '';
-      $scope.link = '';
-    };
-    $scope.incrementUpvotes = function(post) {
-      post.upvotes += 1;
-    };
-  }
-]);
+
+.controller('MainCtrl', ['$scope', 'posts', function($scope, posts){
+  $scope.test = 'Hello world!';
+
+  $scope.posts = posts.posts;
+
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
+    });
+    $scope.title = '';
+    $scope.link = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
+}])
+
+.controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
