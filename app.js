fe13ca32e0e0a4e2cc80e68a425b48971eed56a6
Owen Lamb <ojlamb@github.com.com>
2015-11-10 09:46:28 -0800

    added posts, links and upvotes

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ angular.module('flapperNews', [])
 '$scope',
 function($scope){
   $scope.posts = [
-    'post 1',
-    'post 2',
-    'post 3',
-    'post 4',
-    'post 5'
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
   ];
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0});
+    $scope.title = '';
+    $scope.link = '';
+
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
