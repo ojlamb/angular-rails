7565f44e9d99c369f8f6b84c6dafa870049cf358
Owen Lamb <Owen@owenlambmac.home>
2015-11-10 08:54:03 -0800

    added ng-repeat with mock posts

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', [
 '$scope',
 function($scope){
-  $scope.test = 'Hello world!';
+  $scope.posts = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'
+  ];
 }]);
