fde2442e4f9f34861c5de08024b94599e8458c68
Owen Lamb <owen.lamb@gmail.com>
2015-11-12 10:55:54 -0800

    added comments function

--- app.js
+++ app.js
@@ -54,4 +54,13 @@ function($stateProvider, $urlRouterProvider) {
 
 .controller('PostsCtrl', [ '$scope', '$stateParams', 'posts', function($scope, $stateParams, posts){
   $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+  if($scope.body === '') { return; }
+  $scope.post.comments.push({
+    body: $scope.body,
+    author: 'user',
+    upvotes: 0
+  });
+  $scope.body = '';
+  };
 }]);
