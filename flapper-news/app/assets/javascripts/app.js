8b97f061d4ff6480c466986235d21b2c43e4f932
Owen Lamb <owen.lamb@gmail.com>
2015-11-14 16:22:36 -0800

    added devise auth for user signin register,sign_in.json not yet working

--- flapper-news/app/assets/javascripts/app.js
+++ flapper-news/app/assets/javascripts/app.js
@@ -1,4 +1,4 @@
-angular.module('flapperNews', ['ui.router', 'templates']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+angular.module('flapperNews', ['ui.router', 'templates', 'Devise']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
   $stateProvider.state('home', {
       url: '/home',
@@ -13,6 +13,26 @@ angular.module('flapperNews', ['ui.router', 'templates']).config(['$stateProvide
       url: '/posts/{id}',
       templateUrl: 'posts/_posts.html',
       controller: 'PostsCtrl'
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/_login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+      Auth.currentUser().then(function (){
+        $state.go('home');
+      })
+    }]
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/_register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'Auth', function($state, Auth) {
+      Auth.currentUser().then(function (){
+        $state.go('home');
+      })
+    }]
     });
 
   $urlRouterProvider.otherwise('home');
